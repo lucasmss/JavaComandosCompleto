8dceb13400499c6fddb30dde1ca533159abca332
Lucas Mateus <lucasmss16@gmail.com>
2019-06-18 19:05:30 -0300

    Subindo

--- src/composição/Principal.java
+++ src/composição/Principal.java
@@ -17,54 +17,44 @@ public class Principal {
 		String departamentoName = sc.nextLine();
 		
 		System.out.println("Digite os dados do trabalhador");
-		System.out.println("Nome: ");
+		System.out.print("Nome: ");
 		String name = sc.nextLine();
-		System.out.println("Nivel: ");
+		System.out.print("Nivel: ");
 		String level = sc.nextLine();
-		System.out.println("Salário base: ");
+		System.out.print("Salário base: ");
 		Double salarioBase = sc.nextDouble();
 		
 		Trabalhador Trabalhador = new Trabalhador(name, NivelTrabalho.valueOf(level), salarioBase, new Departamento(departamentoName));	
 		
-		System.out.println("Quantos contratos o trabalhador tem??");
+		System.out.print("Quantos contratos o trabalhador tem??");
 		int n = sc.nextInt();
 		
 		for(int i = 1; i<=n; i++) {
 			
-			System.out.println("Contratos que entraram#"+ i +"dados");
+			System.out.println("Contratos que entraram # "+ i +" dados");
 			System.out.print("Data(DD/MM/YYYY):");
 			Date contratoData = sdf.parse(sc.next());
-			System.out.println("Valor por hora");
+			System.out.print("Valor por hora ");
 			double valorPorHora = sc.nextDouble();
-			System.out.println("Duração em horas");
+			System.out.print("Duração em horas ");
 			int horas = sc.nextInt();
 			
 			HouraContrato contrato = new HouraContrato(contratoData, valorPorHora, horas);
-		
-		
-		
+			Trabalhador.addContrato(contrato);
 		
 		}
 		
+		System.out.println();
+		System.out.print("Entre com a dada e o ano para calcular o salário(MM/YYYY): ");
+		String mesEAno = sc.next();
+		int mes = Integer.parseInt(mesEAno.substring(0, 2));
+		int ano = Integer.parseInt(mesEAno.substring(3));
 		
+		System.out.println("Nome: "+Trabalhador.getName());
+		System.out.println("Departamento: "+Trabalhador.getDepartamaento().getName());
+		System.out.println("Incom for: "+mesEAno+" : "+String.format("%.2f", Trabalhador.incom(ano, mes)));
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
 		
 		sc.close();
 	}
